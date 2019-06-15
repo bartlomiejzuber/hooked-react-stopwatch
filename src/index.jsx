5eee1e478bd96ce4896fe3bd913f3d0b2d895473
Bartlomiej Zuber <bartlomiej.zuber@outlook.com>
2019-06-15 12:28:41 +0200

    cleaning up the code

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import StopWatch from "./StopWatch";
+
+ReactDOM.render(
+  <StopWatch showIcons/>,
+  document.getElementById('root')
+);
