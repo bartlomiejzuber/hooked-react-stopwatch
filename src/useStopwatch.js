028236d82b18b20a7d18c5ebeefb23e9d8cc5c6b
Bartlomiej Zuber <bartlomiej.zuber@outlook.com>
2019-06-18 17:04:24 +0200

    custom hook to handle start-stop-reset of internal interval added

---
+++ src/useStopwatch.js
@@ -0,0 +1,80 @@
+import { useState, useRef, useCallback, useEffect } from "react";
+import moment from "moment";
+import throttle from "lodash/throttle";
+
+export function calculateTimeDiff(startTime, pauseOffset) {
+  let timeDiff = moment.duration(moment().diff(startTime));
+  if (pauseOffset) timeDiff = timeDiff.add(pauseOffset);
+
+  return timeDiff;
+}
+
+const defaultState = {
+  startTime: null,
+  hours: 0,
+  minutes: 0,
+  seconds: 0,
+  milliseconds: 0,
+  pauseOffset: 0,
+};
+export function useStopwatch(initialState = defaultState, throttling = 50) {
+  // State
+  const [time, setTime] = useState(initialState);
+  const intervalHandle = useRef({ timer: null });
+
+  // Initialize
+  useEffect(() => {
+    return () => clearInterval(intervalHandle.current.timer);
+  }, []);
+
+  // Timer start
+  const startHandler = useCallback(() => {
+    if (!intervalHandle.current.timer) {
+      const startTime = moment();
+      const updateTimer = () => {
+        const timeDiff = calculateTimeDiff(startTime, time.pauseOffset);
+        setTime({
+          ...time,
+          startTime,
+          hours: timeDiff.hours(),
+          minutes: timeDiff.minutes(),
+          seconds: timeDiff.seconds(),
+          milliseconds: timeDiff.milliseconds()
+        });
+      };
+      intervalHandle.current.timer = setInterval(throttle(updateTimer, throttling), 10);
+    }
+  }, [time.pauseOffset]);
+
+  // Timer pause
+  const pauseHandler = useCallback(() => {
+    if (intervalHandle.current.timer) {
+      clearInterval(intervalHandle.current.timer);
+      intervalHandle.current.timer = null;
+      const durationToPauseClick = moment.duration(
+        moment().diff(time.startTime)
+      );
+      durationToPauseClick.add(time.pauseOffset);
+      setTime({
+        ...time,
+        pauseOffset: durationToPauseClick
+      });
+    }
+  }, [time]);
+
+  // Timer reset
+  const resetHandler = useCallback(() => {
+    clearInterval(intervalHandle.current.timer);
+    intervalHandle.current.timer = null;
+    setTime(initialState);
+  }, [initialState]);
+
+  return {
+    startHandler,
+    resetHandler,
+    pauseHandler,
+    intervalHandle,
+    time,
+    setTime
+  };
+}
