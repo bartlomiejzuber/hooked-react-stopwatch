21bae63852c11353314780521ab9802aa03964d6
Bartlomiej Zuber <bartlomiej.zuber@outlook.com>
2019-06-20 18:27:34 +0200

    clean whole project before nwb

--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Stopwatch from "./Stopwatch";
-
-ReactDOM.render(
-  <Stopwatch showIcons/>,
-  document.getElementById('root')
-);
