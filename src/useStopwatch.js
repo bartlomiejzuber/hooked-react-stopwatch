08303635bbd98969dad43ade7d2392f7ff303cdd
Bartlomiej Zuber <bartlomiej.zuber@outlook.com>
2019-06-20 19:06:25 +0200

    optimize stopwatch

--- src/useStopwatch.js
+++ src/useStopwatch.js
@@ -1,6 +1,5 @@
 import { useState, useRef, useCallback, useEffect } from "react";
 import moment from "moment";
-import throttle from "lodash/throttle";
 
 export function calculateTimeDiff(startTime, pauseOffset) {
   let timeDiff = moment.duration(moment().diff(startTime));
@@ -15,9 +14,9 @@ const defaultState = {
   minutes: 0,
   seconds: 0,
   milliseconds: 0,
-  pauseOffset: 0,
+  pauseOffset: 0
 };
-export function useStopwatch(initialState = defaultState, throttling = 70) {
+export function useStopwatch(initialState = defaultState, throttling = 50) {
   // State
   const [time, setTime] = useState(initialState);
   const intervalHandle = useRef({ timer: null });
@@ -33,18 +32,18 @@ export function useStopwatch(initialState = defaultState, throttling = 70) {
       const startTime = moment();
       const updateTimer = () => {
         const timeDiff = calculateTimeDiff(startTime, time.pauseOffset);
-        setTime({
-          ...time,
+        setTime(prev => ({
+          ...prev,
           startTime,
           hours: timeDiff.hours(),
           minutes: timeDiff.minutes(),
           seconds: timeDiff.seconds(),
           milliseconds: timeDiff.milliseconds()
-        });
+        }));
       };
-      intervalHandle.current.timer = setInterval(throttle(updateTimer, throttling), 10);
+      intervalHandle.current.timer = setInterval(updateTimer, throttling);
     }
-  }, [time.pauseOffset]);
+  }, [setTime, time, intervalHandle]);
 
   // Timer pause
   const pauseHandler = useCallback(() => {
@@ -55,10 +54,10 @@ export function useStopwatch(initialState = defaultState, throttling = 70) {
         moment().diff(time.startTime)
       );
       durationToPauseClick.add(time.pauseOffset);
-      setTime({
-        ...time,
+      setTime(prev => ({
+        ...prev,
         pauseOffset: durationToPauseClick
-      });
+      }));
     }
   }, [time]);
 
@@ -66,7 +65,7 @@ export function useStopwatch(initialState = defaultState, throttling = 70) {
   const resetHandler = useCallback(() => {
     clearInterval(intervalHandle.current.timer);
     intervalHandle.current.timer = null;
-    setTime(initialState);
+    setTime(() => initialState);
   }, [initialState]);
 
   return {
